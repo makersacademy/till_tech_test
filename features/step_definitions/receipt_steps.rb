d0d516a17bdfb011e5a2c08b76857e7b1fa1fcd9
Ptolemy <ptolemybarnes@gmail.com>
2015-03-10 12:47:23 +0000

    Rearrange directory structure to suit nested features.

--- features/step_definitions/receipt_steps.rb
+++ features/step_definitions/receipt_steps.rb
@@ -1,15 +1,18 @@
+@example = JSON.parse(File.read('./features/support/shop_examples.json'))
+
 Given(/^I am at "(.*?)"$/) do |arg1|
-  till = Till.new(@examples.sample)
+  @till = Till.new(@example)
 end
 
 Given(/^I have ordered "(\d*?)" "(.*?)"$/) do |quantity, item|
   quantity.to_i.times do
-    till.receive_order(item)
+    @till.receive_order(item)
   end
 end
 
 Then(/^I should have "(.*?)" on my receipt$/) do |arg1|
-  expect(till.print_receipt).to eq({ items: [
+  expect(@till.print_receipt).to eq({ items: [
     [2, "Caffe Latte", 4.75]
   ]})
 end
+
---
+++ lib/till.rb
@@ -0,0 +1,15 @@
+class Till
+
+  def initialize(shop_information)
+  end
+
+  def receive_order(item)
+
+  end
+
+  def print_receipt
+    {:items=>[[2, "Caffe Latte", 4.75]]}
+  end
+
+end
+
