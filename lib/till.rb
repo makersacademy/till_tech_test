d4b2319599e26d4f5313fb3df365a4b9a8244193
Luke Clewlow <luke.clewlow@gmail.com>
2015-03-10 20:43:03 +0000

    updated ability to list all different parts needed for  body of reciept

--- lib/till.rb
+++ lib/till.rb
@@ -1,22 +1,45 @@
 class Till
 
-	attr_reader :customer_order, :tax
+	attr_reader :customer_order, :tax, :complete_order
 
 	def initialize
+		@complete_order = {}
 		@customer_order = []
 		@tax = 1.0864
 	end
 
-	def line_total(product_price, quantity)
-		product_price * quantity
+	def line_total(price, quantity)
+		price * quantity
 	end
 
-	def add_line_total_to_order(product_price, quantity)
-		@customer_order << self.line_total(product_price, quantity)
+	def add_line_total_to_order_calc(price, quantity)
+		customer_order << self.line_total(price, quantity)
+	end
+
+	def add_item_to_print_out(name, price, quantity)
+		complete_order.store(name, [quantity, price])
+	end
+
+	def record_item(name, price, quantity)
+		add_item_to_print_out(name, price, quantity)
+		add_line_total_to_order_calc(price, quantity)
+	end
+
+	def list_of_item_names
+		complete_order.keys
+	end
+
+	def list_of_item_quantities
+		quantities = []
+		complete_order.values.collect(&:last)
+	end
+
+	def list_of_line_prices
+		customer_order
 	end
 
 	def subtotal
-		@customer_order.inject(:+)
+		customer_order.inject(:+)
 	end
 
 	def total
@@ -25,6 +48,10 @@ class Till
 
 	def time_and_date
 		Time.now.strftime("%d/%m/%Y %H:%M")
+	end
+
+	def print_out_order
+		puts complete_order
 	end
 
 end
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -12,26 +12,45 @@ describe 'till' do
 	end
 
 	it 'can add a line total to a customers order' do
-		till.add_line_total_to_order("Caffe Latte", 2)
+		till.add_line_total_to_order_calc("Caffe Latte", 2)
 		expect(till.customer_order.length).to eq(1)
 	end
 
 	it 'can calculate the subtotal of an order' do
 		allow(menu).to receive(:options).and_return(4.75,3.85)
-		till.add_line_total_to_order(menu.options("Caffe Latte"), 2)
-		till.add_line_total_to_order(menu.options("Cappucino"), 1)
+		till.add_line_total_to_order_calc(menu.options("Caffe Latte"), 2)
+		till.add_line_total_to_order_calc(menu.options("Cappucino"), 1)
 		expect(till.subtotal).to eq(13.35)
 	end
 
 	it 'can calculate the total of an order inc. tax' do
 		allow(menu).to receive(:options).and_return(4.75,3.85)
-		till.add_line_total_to_order(menu.options("Caffe Latte"), 2)
-		till.add_line_total_to_order(menu.options("Cappucino"), 1)
+		till.add_line_total_to_order_calc(menu.options("Caffe Latte"), 2)
+		till.add_line_total_to_order_calc(menu.options("Cappucino"), 1)
 		expect(till.total).to eq(14.50)
+	end
+
+	it 'can provide a list of the item names ordered' do
+		allow(menu).to receive(:options).and_return(4.75)
+		till.add_item_to_print_out("Caffe Latte", 2, till.line_total(menu.options("Caffe Latte"), 2))
+		expect(till.list_of_item_names).to eq(["Caffe Latte"])
+	end
+
+	it 'can provide a list of the quantities ordered' do
+		allow(menu).to receive(:options).and_return(4.75)
+		till.add_item_to_print_out("Caffe Latte", 2, till.line_total(menu.options("Caffe Latte"), 2))
+		expect(till.list_of_item_quantities).to eq([2])	
 	end
 
 	it 'can provide the time and date' do
 		expect(till.time_and_date).to eq(Time.now.strftime("%d/%m/%Y %H:%M"))
+	end
+
+	xit 'can output the quantity of each item purchased' do
+		allow(menu).to receive(:options).and_return(4.75,3.85)
+		till.add_line_total_to_order_calc(menu.options("Caffe Latte"), 2)
+		till.add_line_total_to_order_calc(menu.options("Cappucino"), 1)
+		expect(till.list_quantities).to eq([2,1])
 	end
 
 end
