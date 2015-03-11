2f9079ca9e723b4d4aec294cb74ffdce87aace46
Josh Bebbington <josh@focalwebsites.com>
2015-03-11 14:37:25 +0000

    Refactors tests to be better structured

--- lib/till.rb
+++ lib/till.rb
@@ -3,6 +3,10 @@ require 'transaction'
 class Till
 
   def initialize
+    new_transaction
+  end
+
+  def new_transaction
     @transaction = Transaction.new
   end
 
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -4,7 +4,12 @@ describe Till do
 
   let(:till) {Till.new}
 
-  it 'Can open a new transaction' do
-    expect(till.transaction.subtotal).to eq 0;
+  context 'Opening and closing a transaction' do
+
+    it 'Can open a new transaction' do
+      expect(till.transaction.subtotal).to eq 0;
+    end
+
   end
+
 end
