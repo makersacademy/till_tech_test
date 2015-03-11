29d90d97535d1ac48ebfc07ed6bb6a9658caf062
marcin walendzik <marcin.walendzik@gmail.com>
2015-03-11 16:12:29 +0000

    adding initializer

--- lib/till.rb
+++ lib/till.rb
@@ -2,5 +2,12 @@ class Till
   
   attr_accessor :tables,:address,:phone,:shopName,:waiters
 
+  def initialize(tables,address,phone,shopName,waiters)
+    @tables = tables;
+    @address = address;
+    @phone = phone
+    @shopName = shopName
+    @waiters = waiters
+  end  
 
 end
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -2,30 +2,26 @@ require 'till'
 
 describe Till do
 
-  let(:till){Till.new}
+  let(:till){Till.new(4,"123 Lakeside Way","16503600708",
+                      "The Coffee Connection",2)}
 
   it "can have the address" do 
-    till.address = "123 Lakeside Way"
     expect(till.address).to eq("123 Lakeside Way")
   end
 
   it "can have a number of tables" do 
-    till.tables = 4
     expect(till.tables).to eq(4)
   end
 
-  it "can have a number" do 
-    till.phone = "16503600708"
+  it "can have a phone number" do 
     expect(till.phone).to eq("16503600708")
   end
 
   it "can have a name of the restaurant" do 
-    till.shopName = "The Coffee Connection"
     expect(till.shopName).to eq("The Coffee Connection")
   end
 
   it "can have a number of waiters taking orders" do 
-    till.waiters = 2
     expect(till.waiters).to eq(2)
   end
 
