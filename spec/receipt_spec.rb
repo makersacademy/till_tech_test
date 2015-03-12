f05d55e9c18ad6d1cfe48496a8dc82f92ca53eba
Luke Clewlow <luke.clewlow@gmail.com>
2015-03-12 21:03:02 +0000

    added discounts. renamed several methods for readability

---
+++ spec/receipt_spec.rb
@@ -0,0 +1,7 @@
+# describe 'receipt' do
+	
+# 	it 'can print all the details of the shop including date and time' do
+# 		expect(receipt.print_details).to eq("#{Time.now.strftime("%d/%m/%Y %H:%M")}")
+# 	end
+
+# end
