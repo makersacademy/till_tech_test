e657bcbec787ca44f1b258b05a041004f58415f5
Ptolemy <ptolemybarnes@gmail.com>
2015-03-13 00:57:03 +0000

    Refactor the way OrderList condenses duplicate order.

--- lib/order_list.rb
+++ lib/order_list.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class OrderList 
   attr_reader :orders
 
@@ -19,10 +20,25 @@ class OrderList
   end
   
   def itemized_list 
-    orders.map do |order|
-      {name: order.name, quantity: orders.select {|ord| ord.name == order.name}.count,
-       cost: order.cost * orders.select {|ord| ord.name == order.name}.count }
+    condense orders.map(&:print)
+  end
+
+  def condense order_hashes
+    order_hashes = condense_by_quantity order_hashes
+    order_hashes = recalculate_cost     order_hashes
+    order_hashes
+  end
+
+  def condense_by_quantity order_hashes
+    order_hashes.map do |order|
+      order[:quantity] = order_hashes.count {|ord| order[:name] == ord[:name] }
+      order
     end.uniq
+  end
+
+  def recalculate_cost order_hashes
+    order_hashes.map {|order| order[:cost] *= order[:quantity] } 
+    order_hashes
   end
  
 end
--- spec/order_list_spec.rb
+++ spec/order_list_spec.rb
@@ -2,8 +2,10 @@ require './lib/order_list'
 
 describe OrderList do
 
-  let(:spaghetti_order) { double('order', cost: 5.0, name: 'spaghetti',:cost= => nil)}
-  let(:coffee_order)    { double('coffee',cost: 3.0, name: 'coffee' ,  :cost= => nil)}
+  let(:spaghetti_order) { double('spaghetti', cost: 5.0, 
+                                 print: {cost: 5.0, name: 'spaghetti'}, :cost= => nil)}
+  let(:coffee_order)    { double('coffee', cost: 3.0, print: {cost: 3.0, name: 'coffee'},
+                                 :cost= => nil)}
   let(:order_list)      { OrderList.new }
 
   describe OrderList do
@@ -15,6 +17,13 @@ describe OrderList do
       expect(order_list.print).to eq ({order: { items: [
           { name: 'spaghetti', quantity: 1, cost:   5.0 },
           { name: 'coffee'   , quantity: 2, cost: 6.0 }], total: 11.0}})
+    end
+
+    it 'condenses identical orders into single orders' do
+      double_order = Array.new(2) { { name: 'coffee', cost: 3.0 } }
+
+      expect(order_list.condense(double_order)).to eq([{ name: 'coffee', cost: 6.0, 
+                                                        quantity: 2 }])
     end
   end
 end
