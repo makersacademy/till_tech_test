7f66eeec91c7c5df5abae9cf40f0b4461f7f65e2
Ptolemy <ptolemybarnes@gmail.com>
2015-03-13 00:07:00 +0000

    Pass discount feature: discounted price will show up in total price on receipt

--- features/step_definitions/receipt_steps.rb
+++ features/step_definitions/receipt_steps.rb
@@ -11,7 +11,9 @@ Given(/^I (?:have ordered|order) "(.*?)"(?:$| at a cost of "(.*?)"$)/) do |order
 end
 
 Given(/^I have order a discounted "(.*?)"$/) do |order|
-  order_list.receive_order Order.new({name: "Muffin", price: 5.0, discount: "5%"}) 
+  @price_to_be_discounted = 5.0
+  order_list.receive_order Order.new({name: "Muffin", price: @price_to_be_discounted,
+                                      discount: "5%"}) 
 end
 
 Then(/^my receipt shows an itemized list of my order$/) do
@@ -27,7 +29,7 @@ Then(/^my receipt shows a tax of "(.*?)"$/) do |tax|
 end
 
 Then(/^my receipt shows a discounted cost$/) do
-  expect(receipt.print[:total] < receipt.print[:order][:total]).to be_true
+  expect(receipt.print[:total] < @price_to_be_discounted).to eq true 
 end
 
 
