44a19ac7562cc64a6b6460cc85a26c59a2c2c36c
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-13 09:22:22 +0000

    adding test - order counts frequency of products

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,14 +5,19 @@ describe 'Order' do
   let(:muffin){double :product, name: "Blueberry Muffin", price: 4.05}
   let(:cappucino){double :product, name: "Cappucino", price: 3.85}
   let(:order){Order.new(muffin)}
-  let(:big_order){Order.new(muffin, cappucino)}
+  let(:medium_order){Order.new(muffin, cappucino)}
+  let(:big_order){Order.new(muffin, cappucino, cappucino)}
   
   it "contains at least one product" do
     expect(order.contents).to eq([muffin])
   end
 
   it "can contain more than one product" do
-    expect(big_order.contents).to eq([muffin, cappucino])
+    expect(medium_order.contents).to eq([muffin, cappucino])
+  end
+
+  it "counts the frequency of products contained" do
+    expect(big_order.number_of(cappucino)).to eq(2)
   end
 
 end
