b16fee1197e9dbbad38ec1218a466c92400f4aa8
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-14 16:43:15 +0000

    adding test - receipt shows which items have been ordered

--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -2,14 +2,17 @@ require 'receipt'
 
 describe "Receipt" do
 
-  let(:order){double :order, items: [{name: "Blueberry Muffin", price: 4.05},{name: "Cappucino", price: 3.85}, {name: "Cappucino", price: 3.85}] }
+  let(:muffin){double :product, name: "Blueberry Muffin", price: 4.05}
+  let(:cappucino){double :product, name: "Cappucino", price: 3.85}
+  let(:order){double :order}
   let(:receipt){Receipt.new(order)}
 
   it "contains an order" do
     expect(receipt.order).to eq(order)
   end
 
-  xit "shows which items have been ordered" do 
+  xit "shows which items have been ordered" do
+    expect(receipt.items).to include("muffin") 
   end
 
   xit "shows how many of each item have been ordered" do
