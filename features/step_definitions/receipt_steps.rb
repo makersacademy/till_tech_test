4a05e6e4ab65daceca5adfa814a91cf7fe5f0457
Ptolemy <ptolemybarnes@gmail.com>
2015-03-14 13:42:17 +0000

    Write step definitions for big spend discount scenario.

--- features/step_definitions/receipt_steps.rb
+++ features/step_definitions/receipt_steps.rb
@@ -9,6 +9,11 @@ Given(/^I am at a cafe with a sales tax of "(.*?)"$/) do |tax|
   receipt.evaluators[:tax] = Tax.new(tax.to_f)
 end
 
+Given(/^I am at a cafe with a discount on orders over "(#{FLOAT})"$/) do |discount|
+  receipt.evaluators[:location] = Location.new(discount: "10%", 
+                                               min_spend_for_discount: discount)
+end
+
 Given(/^I (?:have ordered|order) "(.*?)"(?:$| at a cost of "(#{FLOAT})"$)/) do |order, cost|
   @cost = cost || 3
   order_list.receive_order Order.new({name: order, price: @cost})
@@ -18,6 +23,10 @@ Given(/^I have ordered a discounted "(.*?)"$/) do |order|
   @price_to_be_discounted = 5.0
   order_list.receive_order Order.new({name: "Muffin", price: @price_to_be_discounted,
                                       discount: "5%"}) 
+end
+
+Given(/^I have spent over "(#{FLOAT})"$/) do |total_spent|
+  order_list.receive_order Order.new({name: "Champagne", price: total_spend*2})
 end
 
 Then(/^my receipt shows an itemized list of my order$/) do
@@ -36,5 +45,7 @@ Then(/^my receipt shows a discounted cost$/) do
   expect(receipt.print[:total] < @price_to_be_discounted).to eq true 
 end
 
-
+Then(/^my receipt shows a discounted grand total$/) do
+    pending # express the regexp above with the code you wish you had
+end
 
