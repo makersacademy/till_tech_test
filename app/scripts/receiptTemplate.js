521dcf1542801fb5bf13e594e97ceb7ccbd7fd3f
Ptolemy <ptolemybarnes@gmail.com>
2015-03-14 22:54:20 +0000

    index view displays orders as itemized list.

--- app/scripts/receiptTemplate.js
+++ app/scripts/receiptTemplate.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   console.log('jquery initiated');
 
-  var test_data = jQuery.parseJSON("{\"order\":{\"items\":[{\"name\":\"Champagne\",\"price\":100.0,\"cost\":100.0,\"discount\":\"0%\",\"quantity\":1}],\"total\":100.0},\"discount\":{\"total\":-10.0,\"discount\":\"10%\"},\"total\":90.0}");
+  var test_data = jQuery.parseJSON("{\"order\":{\"items\":[{\"name\":\"Champagne\",\"price\":100.0,\"cost\":100.0,\"discount\":\"0%\",\"quantity\":1},{\"name\":\"Spaghetti\",\"price\":10.0,\"cost\":10.0,\"discount\":\"0%\",\"quantity\":1}],\"total\":110.0},\"discount\":{\"total\":-10.0,\"discount\":\"10%\"},\"total\":90.0}");
+
   console.log(test_data);
 
   var source = $('#receipt-template').html();
