fa86c87674f81542afbeddc658371d3c47e0badd
bibiana <bcristofol@gmail.com>
2015-03-15 14:47:03 +0000

    Added price and added catalogue as an argument to Till

--- js/Till.js
+++ js/Till.js
@@ -1,15 +1,21 @@
-var Till = function() {
+var Till = function(catalogue) {
+  this.catalogue = catalogue;
+  
   this.totalQuantity = 0;
   this.minimumQuantity = 0;
   this.defaultQuantityChange = 1;
+
+  this.prices = catalogue[0]["prices"][0];
+  this.totalPrice = 0;
 };
 
-Till.prototype.addProduct = function(quantityToChangeBy) {
+Till.prototype.addProduct = function(productName, quantityToChangeBy) {
   quantityToChangeBy = quantityToChangeBy || this.defaultQuantityChange;
   this.totalQuantity += quantityToChangeBy;
+  this.totalPrice += this.prices[productName] * quantityToChangeBy;
 };
 
-Till.prototype.removeProduct = function(quantityToChangeBy) {
+Till.prototype.removeProduct = function(productName, quantityToChangeBy) {
   quantityToChangeBy = quantityToChangeBy || this.defaultQuantityChange;
 
   if (this.totalQuantity - quantityToChangeBy < this.minimumQuantity) {
--- spec/TillSpec.js
+++ spec/TillSpec.js
@@ -1,17 +1,38 @@
 describe ("Till", function(){
 
+  var catalogue;
   var till;
   var quantityToChangeBy;
 
   beforeEach(function() {
-    till = new Till();
+    catalogue =
+      [
+        {
+          "shopName": "The Coffee Connection",
+          "address": "123 Lakeside Way",
+          "phone": "16503600708",
+          "prices": [
+            {
+              "Cafe Latte": 4.75,
+              "Flat White": 4.75,
+              "Cappucino": 3.85,
+            }
+          ]
+        }
+      ];
+    till = new Till(catalogue);
     quantityToChangeBy = 1;
+
   });
 
   describe("by default", function() {
 
-    it("total should be 0", function() {
+    it("total quantity should be 0", function() {
       expect(till.totalQuantity).toEqual(0);
+    });
+
+    it("total price should be 0", function() {
+      expect(till.totalPrice).toEqual(0);
     });
 
   });
@@ -19,19 +40,30 @@ describe ("Till", function(){
   describe("products", function() {
 
     it("can add a product", function() {
-      till.addProduct(quantityToChangeBy);
-      expect(till.totalQuantity).toEqual(1);
+      till.addProduct("Cafe Latte", quantityToChangeBy);
+      till.addProduct("Flat White", quantityToChangeBy);
+      expect(till.totalQuantity).toEqual(2);
     });
 
     it("can remove a product", function() {
-      till.addProduct(quantityToChangeBy);
-      till.removeProduct(quantityToChangeBy);
-      expect(till.totalQuantity).toEqual(0);
+      till.addProduct("Cafe Latte", quantityToChangeBy);
+      till.addProduct("Flat White", quantityToChangeBy);
+      till.removeProduct("Cafe Latte", quantityToChangeBy);
+      expect(till.totalQuantity).toEqual(1);
     });
 
     it("cannot be negative", function() {
-      till.removeProduct(quantityToChangeBy);
+      till.removeProduct("Cafe Latte", quantityToChangeBy);
       expect(till.totalQuantity).toEqual(0);
+    });
+
+  });
+
+  describe("prices", function() {
+
+    it("can add the price of the added product", function() {
+      till.addProduct("Cafe Latte", quantityToChangeBy);
+      expect(till.totalPrice).toEqual(4.75)
     });
 
   });
