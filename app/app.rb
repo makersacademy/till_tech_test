e5f705bbcfcb42f025cda2f59f6fdc78aabbd57b
Ptolemy <ptolemybarnes@gmail.com>
2015-03-15 19:32:12 +0000

    Implemented mock menu data into api/cafe/thecafe api endpoint.

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,17 @@
 require 'sinatra/base'
 require 'erb'
+require "sinatra/json"
 
 class TillTechTest < Sinatra::Base
   set :root, File.dirname(__FILE__)
+  helpers Sinatra::JSON
 
   get '/' do
     erb :index 
+  end
+
+  get '/api/menu/thecafe' do
+    json({ spaghetti: {name: 'Spaghetti', price: 5.0 }})
   end
 
 end
