aedbedf4943db56d039daae48cb77fa8087dd1b4
bibiana <bcristofol@gmail.com>
2015-03-15 12:40:38 +0000

    Can add a product plus refactor previous

--- js/Till.js
+++ js/Till.js
@@ -1,3 +1,9 @@
 var Till = function() {
-  this.total = 0;
+  this.totalQuantity = 0;
+  this.defaultQuantityChange = 1;
+};
+
+Till.prototype.addProduct = function(quantityToChangeBy) {
+  quantityToChangeBy = quantityToChangeBy || this.defaultQuantityChange;
+  this.totalQuantity += quantityToChangeBy;
 };
--- spec/TillSpec.js
+++ spec/TillSpec.js
@@ -1,17 +1,27 @@
 describe ("Till", function(){
 
   var till;
+  var quantityToChangeBy;
 
   beforeEach(function() {
     till = new Till();
+    quantityToChangeBy = 1;
   });
 
   describe("by default", function() {
 
     it("total should be 0", function() {
-      expect(till.total).toEqual(0);
+      expect(till.totalQuantity).toEqual(0);
     });
 
+  });
+
+  describe("products", function() {
+
+    it("can add a product", function() {
+      till.addProduct(quantityToChangeBy);
+      expect(till.totalQuantity).toEqual(1);
+    });
   });
 
 });
