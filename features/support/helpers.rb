6c5d4c8f959bb1c12d205e738221455e7b1bd722
Ptolemy <ptolemybarnes@gmail.com>
2015-03-16 10:56:14 +0000

    Refactor step helpers into module.

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -1,21 +1,24 @@
-def order_list
-  @order_list ||= OrderList.new
+module KnowsTheDomain
+  def order_list
+    @order_list ||= OrderList.new
+  end
+
+  def receipt evaluator_objects = {}
+    @receipt    ||= Receipt.new(order_list, evaluator_objects)
+  end
+
+  def menu
+    @menu ||= Menu.new(spaghetti: {name: 'Spaghetti', price: 5.0 })
+  end
+
+  def order=(order_details)
+    @order = Order.new(order_details)
+  end
+
+  def order
+    @order
+  end
 end
 
-def receipt evaluator_objects = {}
-  @receipt    ||= Receipt.new(order_list, evaluator_objects)
-end
-
-def menu
-  @menu ||= Menu.new(spaghetti: {name: 'Spaghetti', price: 5.0 })
-end
-
-def order=(order_details)
-  @order = Order.new(order_details)
-end
-
-def order
-  @order
-end
-
+World(KnowsTheDomain)
 
