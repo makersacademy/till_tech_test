04115cb3f524e81b00b94fc049342c5d25c74fa9
Ptolemy <ptolemybarnes@gmail.com>
2015-03-16 16:22:35 +0000

    Refactor Menu class to take and return an array of menu items, rather than a hash.

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,16 +3,16 @@ require './lib/order'
 class Menu
   attr_reader :items
 
-  def initialize items = {}
+  def initialize items = [] 
     @items = items
   end
 
   def order item_name
-    unless items[item_name] 
+    unless items.select {|item| item[:name] == item_name }.sample
       raise "'#{item_name.to_s.capitalize}' is not on the menu" 
     end
-    
-    items[item_name] 
+   
+    items.select {|item| item[:name] == item_name }.sample
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,10 +2,10 @@ require './lib/menu'
 
 describe Menu do
 
-  let(:menu) { Menu.new(spaghetti: {name: 'Spaghetti', price: 5.0 }) }
+  let(:menu) { Menu.new([{name: 'Spaghetti', price: 5.0 }]) }
 
   it 'manufactures order information' do
-    expect(menu.order(:spaghetti)[:name]).to eq 'Spaghetti'
+    expect(menu.order('Spaghetti')[:name]).to eq 'Spaghetti'
   end
 
   it 'raises an error if ordered item is not on the menu' do
