1595f7141c714abce6ed5492f02f8c2c7f10689f
Ptolemy <ptolemybarnes@gmail.com>
2015-03-16 15:50:02 +0000

    Connected display of menu items on frontend to API.

--- app/app.rb
+++ app/app.rb
@@ -12,7 +12,8 @@ class TillTechTest < Sinatra::Base
   set :static, true
   
   set :order_list, OrderList.new
-  set :menu      , Menu.new({'latte'    => {name: 'Cafe Latte', price: 2.5 }})
+  set :menu      , Menu.new({'latte'    => {name: 'Cafe Latte', price: 2.5 },
+                             'spaghetti'=> {name: 'Spaghetti' , price: 5.0 }})
 
   get '/' do
     send_file './app/views/index.html'
@@ -22,7 +23,7 @@ class TillTechTest < Sinatra::Base
 
   # returns mock data, for now.
   get '/api/location/thecafe/menu/:id' do
-    json(settings.menu.items)
+    json(settings.menu.items.values.to_a)
   end
 
   get '/api/order/:id' do
--- app/public/scripts/menuTemplate.js
+++ app/public/scripts/menuTemplate.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   console.log('menu template initiated');
 
-  $.get('/api/location/thecafe/menu/1', function(data) {
+  $.get('/api/location/thecafe/menu/1', function(menu_items) {
     var source   = $('#menu-template').html();
     var template = Handlebars.compile(source);
-    
+    var data = { menu_items: menu_items } 
+
     $('#menu-container').html(template(data));
   });
 });
