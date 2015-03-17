1d23a35e2063df4b83a5a6f9b461ea6cf350dc25
Ptolemy <ptolemybarnes@gmail.com>
2015-03-17 11:25:42 +0000

    Refactored handlebars template for the receipt out to a separate .handlebars file.

--- app/public/scripts/script.js
+++ app/public/scripts/script.js
@@ -1,21 +1,13 @@
 $(document).ready(function() {
-  var receiptSource   = $('#receipt-template').html();
-  var receiptTemplate = Handlebars.compile(receiptSource);
-
 
   function refreshReceipt() {
     $.get('/api/order/1', function(data) {
-      $('#receipt-container').html(receiptTemplate(data));
+
+      T.render('receiptTemplate', function(receiptTemplate) {
+        $('#receipt-container').html( receiptTemplate(data) );
+      });
     });
   }
-  
-  $('#menu-container').on('click', '.menu-items', function() {
-    var idName = $(this).attr('id');
-    
-    $.post('/api/order/1', { itemname: idName }, function(data) {
-      refreshReceipt();
-    });
-  })
 
   $.get('/api/location/thecafe/menu/1', function(menu_items) {
     var data = { menu_items: menu_items } 
@@ -25,6 +17,16 @@ $(document).ready(function() {
     });
   });
 
+  
+  $('#menu-container').on('click', '.menu-items', function() {
+    var itemName = $(this).attr('id');
+    
+    $.post('/api/order/1', { itemname: itemName }, function(data) {
+      refreshReceipt();
+    });
+  })
+
+  
   refreshReceipt();
 });
 
