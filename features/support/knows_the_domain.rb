21c8ba9fde7b052b1d59212f0aa4f4f2d9b95b21
Ptolemy <ptolemybarnes@gmail.com>
2015-03-17 17:40:51 +0000

    Moved step definition helpers out into separate files relevant to their functions: knows_the_domain.rb, knows_the_ui.rb.

--- features/support/knows_the_domain.rb
+++ features/support/knows_the_domain.rb
@@ -1,4 +1,3 @@
-
 module KnowsTheDomain
 
   def order_list
@@ -39,20 +38,4 @@ module KnowsTheDomain
 
 end
 
-module KnowsTheUserInterface
-
-  def enter_payment_of_value value 
-    fill_in('payment', with: value)
-    click_button 'Make Payment'
-  end
-
-  def payment_has_been_made?
-    expect(page.find('#receipt-container')).to have_content 'Payment'
-  end
-
-  def wait_for_ajax timeout
-    sleep(timeout)
-  end
-
-end
 
