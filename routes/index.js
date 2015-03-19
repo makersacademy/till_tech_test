6762a39d739ad713e86628399e2c837a326ad21c
Hannah Carney <hannahcarneyart@gmail.com>
2015-03-19 19:53:19 +0000

    again again

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-
+var path = require('path');
 
 function routes(app, db) {
   app.get('/', function(req, res) {
-    res.sendFile('path'.join(__dirname, '../views', '/index.html'));
+    res.sendFile(path.join(__dirname, '../views', '/index.html'));
   });
 
   app.post('/items', function(req, res) {
