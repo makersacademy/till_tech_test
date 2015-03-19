c92971d2bfc3b957dd5ebbb527593e01615eed1c
Luke Clewlow <luke.clewlow@gmail.com>
2015-03-19 12:17:21 +0000

    hound just keeps on giving - useful to know all the style guides though as wasnt aware how shoddy it was looking

--- app/lib/till.rb
+++ app/lib/till.rb
@@ -1,28 +1,28 @@
 require_relative "receipt"
 
 class Till
-	include Receipt
+  include Receipt
 
-	attr_reader :tax
+  attr_reader :tax
 
-	def initialize
-		@tax = 1.0864
-	end
+  def initialize
+    @tax = 1.0864
+  end
 
-	def total_of(order, adjustments)
-	  order_after_discount = adjustments.discount(subtotal_of(order))
-	  (order_after_discount * tax).round(2)
-	end
+  def total_of(order, adjustments)
+    order_after_discount = adjustments.discount(subtotal_of(order))
+    (order_after_discount * tax).round(2)
+  end
 
-	def subtotal_of(order)
-		order.values.collect(&:last).inject(:+)
-	end
+  def subtotal_of(order)
+    order.values.map(&:last).inject(:+)
+  end
 
-	def calculate_change(money_given, order_total)
-		money_given - order_total
-	end
+  def calculate_change(money_given, order_total)
+    money_given - order_total
+  end
 
-	def tax_total(order, adjustments)
-		(total_of(order, adjustments) - adjustments.discount(subtotal_of(order))).round(2)
-	end
+  def tax_total(order, adjustments)
+    (total_of(order, adjustments) - adjustments.discount(subtotal_of(order))).round(2)
+  end
 end
