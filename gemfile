e304a856f990dd85563bd2bf306cdc6ac7cf303b
wardymate <chriswarddev@gmail.com>
2015-04-22 21:15:26 +0100

    gemfile and testing set up

---
+++ gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+ruby '2.2.0'
+
+gem 'rspec'
+gem 'capybara'
---
+++ spec/features/create_receipt_spec.rb
@@ -0,0 +1,4 @@
+feature 'As a customer I want to be able to order items from a coffee shop' do
+
+  xscenario 'jane orders 2 cafe lattes'
+end
