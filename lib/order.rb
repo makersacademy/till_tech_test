81fc9adbb3f9817a7e2c49d99152d7fb0b5071df
Joe Newman <makerslaptop23@Makerss-MBP.home>
2015-04-25 07:12:27 +0100

    total price calculated

--- lib/order.rb
+++ lib/order.rb
@@ -6,21 +6,31 @@ class Order
     @current_order = Hash.new { |l, k| l[k] = Hash.new(&l.default_proc) }
   end
 
-  def add_item_to_order(item, quantity=1, customer)
+  def add_item_to_order(customer, item, quantity=1)
     @current_order[customer][item] = quantity
   end
 
-  def increase_item_quantity(item, quantity=1, customer)
+  def increase_item_quantity(customer, item, quantity=1)
     @current_order[customer][item] += quantity
   end
 
-  def decrease_item_quantity(item, quantity=1, customer)
-    @current_order[customer][item] > 0 ? @current_order[customer][item] -= quantity : raise "already zero"
+  def decrease_item_quantity(customer, item, quantity=1)
+    if @current_order[customer][item] >= quantity
+      @current_order[customer][item] -= quantity
+      else
+    raise "quantity can't be negative"
+    end
+  end
+
+  def total_price_per_customer(customer)
+    total = 0
+    @current_order[customer].each {|item, qty| total += item.price * qty }
+    total
   end
 
   def total_pretax_price
     total = 0
-    current_order.each {|item, qty| total += item.price * qty }
+    @current_order.each {|customer, item, quantity| total += total_price_per_customer(customer)}
     total
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -5,22 +5,54 @@ describe 'Order' do
 
   let(:order) {Order.new}
   let(:item) {double('Item')}
+  let(:customer1) {double('Customer', :name => "Joe")}
+  let(:customer2) {double('Customer', :name => "Bob")}
   let(:item1) {double('Item', :price => 3)}
   let(:item2) {double('Item', :price => 4)}
 
   it "can have an item added to the current order" do
-    order.add_to_order(item)
-    expect(order.current_order).to include(item)
+    order.add_item_to_order(customer1, item)
+    expect(order.current_order[customer1]).to include(item)
   end
 
   it "knows the quantity for each item in the order" do
-    order.add_to_order(item, 2)
-    expect(order.current_order).to include(item => 2)
+    order.add_item_to_order(customer1,item, 2)
+    expect(order.current_order[customer1]).to include(item => 2)
   end
 
-  it "can calculate the total pre-tax price" do
-    order.add_to_order(item1, 2)
-    order.add_to_order(item2, 3)
+  it "can add items for different customers" do
+    order.add_item_to_order(customer1, item1)
+    order.add_item_to_order(customer2, item2)
+    expect(order.current_order[customer1]).to include(item1)
+    expect(order.current_order[customer2]).to include(item2)
+  end
+
+  it "can increase the item quantity" do
+    order.add_item_to_order(customer1, item, 2)
+    order.increase_item_quantity(customer1, item)
+    expect(order.current_order[customer1]).to include(item => 3)
+  end
+
+  it "can decrease the item quantity" do
+    order.add_item_to_order(customer1, item, 2)
+    order.decrease_item_quantity(customer1, item)
+    expect(order.current_order[customer1]).to include(item => 1)
+  end
+
+  it "can't decrease the item quantity below zero" do
+    order.add_item_to_order(customer1, item)
+    expect{order.decrease_item_quantity(customer1, item, 2)}.to raise_error
+  end
+
+  it "can calculate the total (pre-tax) price for a customer" do
+    order.add_item_to_order(customer1,item1, 2)
+    order.add_item_to_order(customer1,item2, 3)
+    expect(order.total_price_per_customer(customer1)).to eq(18)
+  end
+
+  it "can calculate the overall total pre-tax price" do
+    order.add_item_to_order(customer1,item1, 2)
+    order.add_item_to_order(customer2,item2, 3)
     expect(order.total_pretax_price).to eq(18)
   end
 end
