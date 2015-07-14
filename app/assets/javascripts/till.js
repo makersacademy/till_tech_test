1feab2d7ce7124be2a853da72c2ff59dfdf0c7ca
Daryl Watson <darylwtsn@gmail.com>
2015-07-14 20:38:44 +0100

    Added other buttons in

--- app/assets/javascripts/till.js
+++ app/assets/javascripts/till.js
@@ -1,6 +1,6 @@
+  var items;
 $(document).ready(function(){
 
-  var items;
   var order = [];
 
   $.getJSON('/hipstercoffee.json', function(data) {
@@ -8,14 +8,25 @@ $(document).ready(function(){
   })
   .done(function() {
     $('#shop-name').text(items[0].shopName);
-  })
+    items = Object.keys(items[0].prices[0])
+    for (i = 0; i < items.length; i ++) {
+      var button = $('<button/>').attr({
+        class: "shop-items",
+        value: items[i]
+      });
+      button.html(items[i])
+      $('body').append(button);
+    }
+  });
 
-  $('#flat-white').click(function() {
-    $('#display').text('4.75');
+  $('body').on('click', '.shop-items', function() {
+    var button = $(this)
+    var item = button[0].value
+    // $('#display').text('4.75');
       $.ajax({ url: '/till',
       type: 'POST',
       beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-      data: { "item" : 'Flat White' }
+      data: { "item" : item }
     });
   });
 
