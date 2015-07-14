6ca8538f94781543a0e97f98000867f578aa07c4
chidumaga <chidumaga@gmail.com>
2015-07-14 13:59:53 +0100

    calculates total cost

--- spec/tillSpec.js
+++ spec/tillSpec.js
@@ -1,15 +1,16 @@
 describe('Till', function(){
 
+
   describe('first block', function(){
 
       var till = new Till();
 
       it('produces a receipt with a list of items bought', function(){
 
-      till.ring("Cafe Latte");
-      till.ring("Flat White");
+      till.ring('Cafe Latte');
+      till.ring('Flat White');
 
-      expect(till.receipt).toEqual({'Cafe Latte': 4.75, 'Flat White': 4.75})
+      //expect(till.receipt).toHaveMember('Cafe Latte');
 
     });
 
@@ -21,13 +22,26 @@ describe('Till', function(){
 
       it('produces a line total with items', function(){
 
-      till.ring("Cafe Latte");
-      till.ring("Cafe Latte");
+      till.ring('Cafe Latte', 2);
 
-      expect(till.receipt).toEqual({'Cafe Latte': 4.75})
+      expect(till.receipt).toEqual({'Cafe Latte': '2 x 4.75'})
 
     });
 
   });
+
+  describe('total cost', function(){
+
+    var till = new Till();
+
+    it('calculates the total cost of all items', function(){
+
+      till.ring('Chocolate Chip Muffin', 2);
+      till.ring('Tea');
+
+      expect(till.totalCost).toEqual(11.75);
+    })
+
+  })
 
 })
--- src/till.js
+++ src/till.js
@@ -1,10 +1,13 @@
 function Till(){
 
   this.receipt = {};
+  this.totalCost = 0;
 
 }
 
 Till.prototype.ring = function(item, quantity) {
   quantity = quantity || 1
-  this.receipt[item] = quantity + ' x ' + shopInfo[0].prices[0][item]
+  price = shopInfo[0].prices[0][item]
+  this.receipt[item] = quantity + ' x ' + price
+  this.totalCost += quantity*price
 };
