b0c6f86ef685609174509da0435e8ddbaf609fff
Alex Handy <handy437@gmail.com>
2015-07-15 10:23:52 +0100

    Reversed menu storage to hold locally to test iPhone deployment

--- till_tech/www/js/tillTechCtrl.js
+++ till_tech/www/js/tillTechCtrl.js
@@ -7,12 +7,39 @@ hiptillio.controller('HipTillioController', ['$http', 'GetShopDetails', function
   self.customerDiscount = 0
   var taxRate = 0.0864
 
-  GetShopDetails.success(function(data) {
-    self.menu = data
-    console.log(self.menu)
-  }).error(function(data, status){
-      console.log("Error")
-  });
+  self.menu = [
+  {
+    "shopName": "The Coffee Connection",
+    "address": "123 Lakeside Way",
+    "phone": "16503600708",
+    "prices": [
+      {
+        "Cafe Latte": 4.75,
+        "Flat White": 4.75,
+        "Cappucino": 3.85,
+        "Single Espresso": 2.05,
+        "Double Espresso": 3.75,
+        "Americano": 3.75,
+        "Cortado": 4.55,
+        "Tea": 3.65,
+        "Choc Mudcake": 6.40,
+        "Choc Mousse": 8.20,
+        "Affogato": 14.80,
+        "Tiramisu": 11.40,
+        "Blueberry Muffin": 4.05,
+        "Chocolate Chip Muffin": 4.05,
+        "Muffin Of The Day": 4.55
+      }
+    ]
+  }
+]
+
+  // GetShopDetails.success(function(data) {
+  //   self.menu = data
+  //   console.log(self.menu)
+  // }).error(function(data, status){
+  //     console.log("Error")
+  // });
 
   self.addItem = function(newItem, quantity) {
     self.newItem = {itemName: newItem, price: self.menu[0].prices[0][newItem]}
