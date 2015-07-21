58de257faaafd7bd3c52bfcabf0825a8dc9987af
Rocco <roccorighi@hotmail.com>
2015-07-21 16:37:16 +0100

    adds changes prior to checking out to test travis set up

--- spec/tillSpec.js
+++ spec/tillSpec.js
@@ -1,4 +1,4 @@
-describe ('Till', function(){
+describe ("Till", function(){
   var till;
   var details;
 
@@ -25,60 +25,60 @@ describe ('Till', function(){
 
   });
 
-  it('has a total of 0 when initialised', function(){
+  it("has a total of 0 when initialised", function(){
     expect(till.total).toEqual(0);
   });
 
-  it ('can calculate the total for one product', function(){
+  it ("can calculate the total for one product", function(){
     till.calculateTotal("Cafe Latte");
     expect(till.total).toEqual(4.75);
   });
 
-  it ('can calculate the total for 2 products', function(){
+  it ("can calculate the total for 2 products", function(){
     till.calculateTotal("Flat White");
     till.calculateTotal("Cafe Latte");
     expect(till.total).toEqual(9.5);
   });
 
-  it ('can calculate the total for 2 orders of the same product', function(){
+  it ("can calculate the total for 2 orders of the same product", function(){
     till.calculateTotal("Cappucino");
     till.calculateTotal("Cappucino");
     expect(till.total).toEqual(7.7);
   });
 
-  it ('can calculate the taxes on the total', function(){
+  it ("can calculate the taxes on the total", function(){
     till.calculateTotal("Cappucino");
     till.calculateTotal("Cappucino");
     till.calculateTaxes();
     expect(till.taxes).toEqual(0.67);
   });
 
-  it ('can show a list of ordered drinks', function(){
+  it ("can show a list of ordered drinks", function(){
     till.calculateTotal("Cappucino");
     till.calculateTotal("Cappucino");
     till.calculateTotal("Flat White");
-    expect(till.order['Cappucino'].quantity).toEqual(2);
+    expect(till.order["Cappucino"].quantity).toEqual(2);
   });
 
-  it ('knows how much change to return the customer', function(){
+  it ("knows how much change to return the customer", function(){
     till.calculateTotal("Cappucino");
     till.calculateTotal("Cappucino");
     var note = 20;
     expect(till.calculateChange(note)).toEqual(12.3);
   });
 
-  it ('can apply a discount for orders over $50', function(){
+  it ("can apply a discount for orders over $50", function(){
     multipleOrder("Flat White", 10);
     expect(till.total).toEqual(49.64);
   });
 
-  it ('can apply a discount to specific items (muffins)', function() {
+  it ("can apply a discount to specific items (muffins)", function() {
     till.calculateTotal("Blueberry Muffin");
     till.calculateTotal("Blueberry Muffin");
     expect(till.total).toEqual(7.28);
   });
 
-  it ('can apply a discount to specific items (muffins) and for orders over $50', function() {
+  it ("can apply a discount to specific items (muffins) and for orders over $50", function() {
     multipleOrder("Blueberry Muffin", 2)
     multipleOrder("Flat White", 5)
     multipleOrder("Double Espresso", 7)
@@ -87,35 +87,9 @@ describe ('Till', function(){
 
 
 
-var multipleOrder = function(item, qty) {
-  for (var i = 0; i <= qty; i++) {
-  till.calculateTotal(item);
-  }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  var multipleOrder = function(item, qty) {
+    for (var i = 0; i <= qty; i++) {
+    till.calculateTotal(item);
+    }
+  };
 });
