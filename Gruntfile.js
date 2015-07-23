81d243b16dafd0f0b7b1b072f2d42959dd36866c
Rocco <roccorighi@hotmail.com>
2015-07-23 13:44:38 +0100

    adds Gruntfile and grunt task to run jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
 jasmine: {
-      components: {
-        src: [
-        'components/*js'
-        ],
-        options: {
-          specs: 'spec/*Spec.js',
-          keepRunner : true,
-          //helpers: 'test/spec/*.js'
-        }
-      },
-       grunt.registerTask('travis', [
+        components: {
+          src: [
+          'components/*js'
+          ],
+          options: {
+            specs: 'tests/spec/*Spec.js',
+            keepRunner : true,
+            //helpers: 'test/spec/*.js'
+          }
+        },
+        grunt.registerTask('travis', [
         'jshint','jasmine'
     ]);
-     };
+}
