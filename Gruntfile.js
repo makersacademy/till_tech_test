d0171dcaa3c18d0ded8fc145a114e64757f3b7ec
Rocco <roccorighi@hotmail.com>
2015-07-23 18:00:07 +0100

    Initialises Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,24 @@
-jasmine: {
-        components: {
-          src: [
-          'components/*js'
-          ],
-          options: {
-            specs: 'spec/*Spec.js',
-            keepRunner : true,
-            //helpers: 'test/spec/*.js'
-          }
-        },
-        grunt.registerTask('travis', [
-        'jshint','jasmine'
-    ]);
-}
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+     jasmine: {
+      components: {
+        src: [
+        'components/*js'
+        ],
+        options: {
+          specs: 'spec/*Spec.js',
+          keepRunner : true,
+          //helpers: 'test/spec/*.js'
+        }
+      }
+    }
+
+  });
+  // Default task.
+  grunt.registerTask('default', ['jasmine']);
+
+};
