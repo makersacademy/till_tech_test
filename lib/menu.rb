22bd295bb724f9e685987f30d02037096b1643f7
TJQKAS <A26052013@GMAIL.COM>
2015-11-17 18:29:21 +0000

    we can set discount and show it separetaly

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,47 +2,60 @@ require 'json'
 
 class Menu
 
-  attr_accessor :customer_bill, :customer_order, :taxes, :sum
-  attr_reader :menu
+     attr_accessor :customer_bill, :customer_order, :taxes, :sum
+     attr_reader :menu
 
   def initialize
-    file = open("hipstercoffee.json")
-    json = file.read
-    @menu = JSON.parse(json)
-    @customer_order = []
-    @customer_bill = []
-    @total = 0
-    @taxes = 0
-    @quantity = 1
+       file = open("hipstercoffee.json")
+       json = file.read
+       @menu = JSON.parse(json)
+       @customer_order = []
+       @customer_bill = []
+       @total = 0
+       @taxes = 0
+       @quantity = 1
 
   end
 
   def order(booking, quantity)
-  @menu[0]["prices"][0].select do |key,value|
-    if booking == key
-      @customer_order.push key => value * quantity
+       @menu[0]["prices"][0].select do |key,value|
+          if booking == key
+       @customer_order.push key => value * quantity
     end
   end
 end
 
 
   def prices
-    @customer_order.each do |hash|
+      @customer_order.each do |hash|
       @customer_bill << hash.values[0]
-    end
-    @customer_bill
+  end
+      @customer_bill
   end
 
 
   def sum
-    sum_bill = @customer_bill.inject(0) { |result, element| result + element }
-    @sum = sum_bill
+      sum_bill = @customer_bill.inject(0) { |result, element| result + element}
+      if sum_bill > 50
+      sum_bill = sum_bill * 0.95
+      @sum = sum_bill.round(2)
+      else
+      @sum = sum_bill.round(2)
+      end
   end
 
   def taxes
-    sum = self.sum
-    taxes = sum * 0.0864
-    @taxes = taxes.round(2)
+      sum = self.sum
+      taxes = sum * 0.0864
+      @taxes = taxes.round(2)
   end
+
+  def discount
+     sum = self.sum
+     without_discount = @customer_bill.inject(0) { |result, element| result + element}
+     @discount = (without_discount - sum).round(2)
+  end
+
+
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -17,22 +17,43 @@ describe Menu do
   end
 
   it "3. We can see the final sum of the bill" do
-    subject.order("Affogato", 10)
-    subject.order("Muffin Of The Day", 10)
+    subject.order("Affogato", 2)
+    subject.order("Muffin Of The Day", 2)
     subject.prices
     subject.sum
-    expect(subject.sum).to be(193.5)
+    expect(subject.sum).to be(38.7)
   end
 
   it "4. We can see the tax of the final sum of the bill" do
-    subject.order("Affogato", 10)
-    subject.order("Muffin Of The Day", 10)
+    subject.order("Affogato", 2)
+    subject.order("Muffin Of The Day", 1)
     subject.prices
     subject.taxes
-    expect(subject.taxes).to be(16.72)
+    expect(subject.taxes).to be(2.95)
   end
 
-  it "5. We can get discount in case our purchase is more than 50$"
+  it "5. We can see price with 5% discount in case our purchase is more than 50$" do
+    subject.order("Affogato", 20)
+    subject.order("Muffin Of The Day", 10)
+    subject.prices
+    expect(subject.sum).to be(324.43)
+  end
+
+   it "6. We can see the discount separetaly in our bill" do
+     subject.order("Affogato", 20)
+     subject.order("Muffin Of The Day", 10)
+     subject.prices
+     expect(subject.discount).to be(17.07)
+  end
+
+  it "7. We can see the change of the customer" do
+    
+  end
+
+
+
+
+
 
 
 end
