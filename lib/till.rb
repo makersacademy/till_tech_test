c1859d1205ece25c42ab565f29e03c3785ee4b93
Anindya Bhattacharyya <bat020@gmail.com>
2015-11-17 14:47:30 +0000

    refactored code and tests

--- lib/till.rb
+++ lib/till.rb
@@ -1,42 +1,41 @@
 class Till
 
-  def self.total_before_tax
-    order = customer_order[:order]
-    prices = coffee_shop[:prices]
-
-    total = 0
-
-    order.each do |item, quantity|
-      total += quantity * prices[item]
-    end
-    return total
-  end
-
-  def self.bill_tax
-    tax = coffee_shop[:tax]
-    return (tax * total_before_tax).round(2)
-  end
-
-  def self.line_entries
-    order = customer_order[:order]
-    prices = coffee_shop[:prices]
-
-    order.map {|item, quantity| [item, {:quantity=> quantity, :item_price=>prices[item], :line_total=>quantity * prices[item]}]}.to_h
-  end
-
-
-
-  # Getting and setting menus/order
   def self.coffee_shop=(shop)
     @coffee_shop = shop
   end
 
-  def self.coffee_shop
-    @coffee_shop
-  end
-
   def self.customer_order=(order)
     @customer_order = order
+  end
+
+  def self.line_entries
+    prices = coffee_shop[:prices]
+    customer_order[:order].map { |item, quantity| [item, {
+      quantity: quantity,
+      item_price: prices[item],
+      line_total: quantity * prices[item] }] }.to_h
+  end
+
+  def self.total_before_tax
+    line_entries.to_a.map { |item| item[1][:line_total] }.inject(&:+)
+  end
+
+  def self.bill_tax
+    (coffee_shop[:taxRate] * total_before_tax).round(2)
+  end
+
+  def self.receipt
+    return {
+      items: line_entries,
+      total_before_tax: total_before_tax,
+      tax: bill_tax,
+      total_inc_tax: total_before_tax + bill_tax }
+  end
+
+private
+
+  def self.coffee_shop
+    @coffee_shop
   end
 
   def self.customer_order
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -1,48 +1,47 @@
-require 'spec_helper'
-require './lib/till.rb'
+require 'till'
 
 describe Till do
 
   subject = described_class
 
-  it 'reads coffee shop details in JSON format' do
-    shop = { "shopName": "Happy Coffe Shop", "prices": { "Latte": 4, "Flat White": 3 }, "tax": 0.0864 }
-    subject.coffee_shop = shop
-    expect(subject.coffee_shop).to eq shop
-  end
+  let(:coffee_shop) { {
+    shopName: "Happy Coffee Shop",
+    prices: { "Latte": 4, "Flat White": 3 },
+    taxRate: 0.0864 } }
 
-  it "reads customer's details in JSON format" do
-    customer_order = { "customerName": "Jane", "order": { "Latte": 1, "Flat White": 2} }
+  let(:customer_order) { {
+    customerName: "Jane",
+    order: { "Latte": 1, "Flat White": 2 } } }
+
+  let(:line_entries) { {
+    "Latte": { quantity: 1, item_price: 4, line_total: 4 },
+    "Flat White": { quantity: 2, item_price: 3, line_total: 6 } } }
+
+  let(:receipt) { {
+    items: line_entries,
+    total_before_tax: 10,
+    tax: 0.86,
+    total_inc_tax: 10.86 } }
+
+  before do
+    subject.coffee_shop = coffee_shop
     subject.customer_order = customer_order
-    expect(subject.customer_order).to eq customer_order
   end
 
   it "produces line totals" do
-    shop = { "shopName": "Happy Coffe Shop", "prices": { "Latte": 4, "Flat White": 3 }, "tax": 0.0864 }
-    customer_order = { "customerName": "Jane", "order": { "Latte": 1, "Flat White": 2} }
-    subject.coffee_shop = shop
-    subject.customer_order = customer_order
-
-    lines = { "Latte": { "quantity": 1, "item_price": 4, "line_total": 4 }, "Flat White": { "quantity": 2, "item_price": 3, "line_total": 6 } }
-    expect(subject.line_entries).to eq lines
+    expect(subject.line_entries).to eq line_entries
   end
 
   it "calculates the total price of the order" do
-    shop = { "shopName": "Happy Coffe Shop", "prices": { "Latte": 4, "Flat White": 3 }, "tax": 0.0864 }
-    customer_order = { "customerName": "Jane", "order": { "Latte": 1, "Flat White": 2} }
-    subject.coffee_shop = shop
-    subject.customer_order = customer_order
     expect(subject.total_before_tax).to eq 10
   end
 
   it "calculates the tax of an order" do
-    shop = { "shopName": "Happy Coffe Shop", "prices": { "Latte": 4, "Flat White": 3 }, "tax": 0.0864 }
-    customer_order = { "customerName": "Jane", "order": { "Latte": 1, "Flat White": 2} }
-    subject.coffee_shop = shop
-    subject.customer_order = customer_order
     expect(subject.bill_tax).to eq 0.86
   end
 
-
+  it "produces a receipt" do
+    expect(subject.receipt).to eq receipt
+  end
 
 end
