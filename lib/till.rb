355abc95a9dfcc515ebdf9abcaa6dcdc2d637591
Anindya Bhattacharyya <bat020@gmail.com>
2015-11-17 18:42:40 +0000

    payments, change, discounts all working

--- lib/till.rb
+++ lib/till.rb
@@ -10,27 +10,67 @@ class Till
 
   def self.line_entries
     prices = coffee_shop[:prices]
-    customer_order[:order].map { |item, quantity| [item, {
-      quantity: quantity,
-      item_price: prices[item],
-      line_total: quantity * prices[item]
-    }] }.to_h
+    customer_order[:order].map { |item, quantity|
+      [item, {
+        quantity: quantity,
+        item_price: prices[item],
+        line_total: quantity * prices[item]
+      }]
+    }.to_h
+  end
+
+  def self.total_before_discounts
+    line_entries.to_a.map { |item| item[1][:line_total] }.inject(&:+).round(2)
+  end
+
+  def self.discounts
+    discounts = {}
+    prices = coffee_shop[:prices]
+    item_discounts = coffee_shop[:itemDiscounts]
+    bill_discount = coffee_shop[:billDiscount]
+    threshold = bill_discount[:threshold]
+    customer_order[:order].each do |item, quantity|
+      rate = item_discounts[item]
+      if rate
+        item_discount = (quantity * prices[item] * rate).round(2)
+        key = "#{(rate*100).round}% #{item} discount".to_sym
+        discounts[key] = item_discount
+      end
+    end
+    if total_before_discounts > threshold
+      rate = bill_discount[:rate]
+      key = "#{(rate*100).round}% off bill over $#{threshold}".to_sym
+      discounts[key] = (total_before_discounts * rate).round(2)
+    end
+    discounts
   end
 
   def self.total_before_tax
-    line_entries.to_a.map { |item| item[1][:line_total] }.inject(&:+)
+    total_discounts = discounts.to_a.map { |item| item[1] }.inject(&:+).round(2)
+    total_before_discounts - total_discounts
   end
 
   def self.bill_tax
     (coffee_shop[:taxRate] * total_before_tax).round(2)
   end
 
+  def self.total_inc_tax
+    (total_before_tax + bill_tax).round(2)
+  end
+
+  def self.change
+    (customer_order[:payment] - bill_tax - total_before_tax).round(2)
+  end
+
   def self.receipt
-    {
-      items: line_entries,
+    { items: line_entries,
+      total_before_discounts: total_before_discounts,
+      discounts: discounts,
       total_before_tax: total_before_tax,
       tax: bill_tax,
-      total_inc_tax: total_before_tax + bill_tax
+      total_inc_tax: total_inc_tax,
+      payment: customer_order[:payment],
+      change: change
     }
   end
 
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -5,33 +5,44 @@ describe Till do
   subject = described_class
 
   let(:coffee_shop) do
-    {
-      shopName: "Happy Coffee Shop",
-      prices: { "Latte": 4, "Flat White": 3 },
-      taxRate: 0.0864
+    { shopName: "Happy Coffee Shop",
+      prices: { "Cafe Latte": 4, "Flat White": 3, "Muffin": 1, "Steak": 20 },
+      taxRate: 0.0864,
+      itemDiscounts: { "Muffin": 0.1, "Flapjack": 0.2 },
+      billDiscount: { threshold: 50, rate: 0.05 }
     }
   end
 
   let(:customer_order) do
-    {
-      customerName: "Jane",
-      order: { "Latte": 1, "Flat White": 2 }
+    { customerName: "Jane",
+      order: { "Cafe Latte": 1, "Flat White": 2, "Muffin": 3, "Steak": 2 },
+      payment: 60
     }
   end
 
   let(:line_entries) do
-    {
-      "Latte": { quantity: 1, item_price: 4, line_total: 4 },
-      "Flat White": { quantity: 2, item_price: 3, line_total: 6 }
+    { "Cafe Latte": { quantity: 1, item_price: 4, line_total: 4 },
+      "Flat White": { quantity: 2, item_price: 3, line_total: 6 },
+      "Muffin": { quantity: 3, item_price: 1, line_total: 3 },
+      "Steak": { quantity: 2, item_price: 20, line_total: 40 },
+    }
+  end
+
+  let(:discounts) do
+    { "10% Muffin discount": 0.3,
+      "5% off bill over $50": 2.65
     }
   end
 
   let(:receipt) do
-    {
-      items: line_entries,
-      total_before_tax: 10,
-      tax: 0.86,
-      total_inc_tax: 10.86
+    { items: line_entries,
+      total_before_discounts: 53,
+      discounts: discounts,
+      total_before_tax: 50.05,
+      tax: 4.32,
+      total_inc_tax: 54.37,
+      payment: 60,
+      change: 5.63
     }
   end
 
@@ -44,12 +55,28 @@ describe Till do
     expect(subject.line_entries).to eq line_entries
   end
 
-  it "calculates the total price of the order" do
-    expect(subject.total_before_tax).to eq 10
+  it "calculates the total before discounts" do
+    expect(subject.total_before_discounts).to eq 53
   end
 
-  it "calculates the tax of an order" do
-    expect(subject.bill_tax).to eq 0.86
+  it "calculates the discounts" do
+    expect(subject.discounts).to eq discounts
+  end
+
+  it "calculates the total before tax" do
+    expect(subject.total_before_tax).to eq 50.05
+  end
+
+  it "calculates the tax on the bill" do
+    expect(subject.bill_tax).to eq 4.32
+  end
+
+  it "calculates the total including tax" do
+    expect(subject.total_inc_tax).to eq 54.37
+  end
+
+  it "calculates the correct change" do
+    expect(subject.change).to eq 5.63
   end
 
   it "produces a receipt" do
