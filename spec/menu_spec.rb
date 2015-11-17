9e351246faed6ae342786b7094544cde6d7c299c
TJQKAS <A26052013@GMAIL.COM>
2015-11-17 19:43:45 +0000

    another commit

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -21,7 +21,7 @@ describe Menu do
     subject.order("Muffin Of The Day", 2)
     subject.prices
     subject.sum
-    expect(subject.sum).to be(38.7)
+    expect(subject.sum).to eq(38.7)
   end
 
   it "4. We can see the tax of the final sum of the bill" do
@@ -29,21 +29,21 @@ describe Menu do
     subject.order("Muffin Of The Day", 1)
     subject.prices
     subject.taxes
-    expect(subject.taxes).to be(2.95)
+    expect(subject.taxes).to eq(2.95)
   end
 
   it "5. We can see price with 5% discount in case our purchase is more than 50$" do
     subject.order("Affogato", 20)
     subject.order("Muffin Of The Day", 10)
     subject.prices
-    expect(subject.sum).to be(324.43)
+    expect(subject.sum).to eq(324.43)
   end
 
    it "6. We can see the discount separetaly in our bill" do
      subject.order("Affogato", 20)
      subject.order("Muffin Of The Day", 10)
      subject.prices
-     expect(subject.discount).to be(17.07)
+     expect(subject.discount).to eq(17.07)
   end
 
    it "7. We can see the change of the customer" do
@@ -52,7 +52,7 @@ describe Menu do
     subject.prices
     subject.sum
     subject.money(600)
-    expect(subject.charge).to be(275.57)
+    expect(subject.charge).to eq(275.57)
   end
 
 end
