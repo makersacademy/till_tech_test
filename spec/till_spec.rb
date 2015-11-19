347a8ee3a1c308e805ac363f33e5a15e4a184a61
Anindya Bhattacharyya <bat020@gmail.com>
2015-11-19 18:43:00 +0000

    added rockets to get travis working

--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -6,35 +6,35 @@ describe Till do
 
   let(:coffee_shop) do
     {
-      prices: {"Cafe Latte": 400, "Flat White": 300,
-        "Muffin": 100, "Steak": 2000},
+      prices: { "Latte" => 400, "Flat White" => 300,
+        "Muffin" => 100, "Steak" => 2000},
       tax_rate: 0.0864,
-      discounts: {"Muffin": 0.1, "Flat White": 0.5},
+      discounts: {"Muffin" => 0.1, "Flat White" => 0.5},
       bill_discount:  {threshold: 5000, rate: 0.05}
     }
   end
 
   let(:customer_order) do
     {
-      order: {"Cafe Latte": 1, "Flat White": 2, "Muffin": 3, "Steak": 2},
+      order: {"Latte" => 1, "Flat White" => 2, "Muffin" => 3, "Steak" => 2},
       payment: 6000
     }
   end
 
   let(:items) do
     {
-      "Cafe Latte": {quantity: 1, price: 400, sum: 400},
-      "Flat White": {quantity: 2, price: 300, sum: 600},
-      "Muffin": {quantity: 3, price: 100, sum: 300},
-      "Steak": {quantity: 2, price: 2000, sum: 4000}
+      "Latte" => {quantity: 1, price: 400, sum: 400},
+      "Flat White" => {quantity: 2, price: 300, sum: 600},
+      "Muffin" => {quantity: 3, price: 100, sum: 300},
+      "Steak" => {quantity: 2, price: 2000, sum: 4000}
     }
   end
 
   let(:discounts) do
     {
-      "Muffin": {rate: 0.1, amount: 30},
-      "Flat White": {rate: 0.5, amount: 300},
-      bill: {rate: 0.05, threshold: 5000, amount: 265}
+      "Muffin" => {rate: 0.1, amount: 30},
+      "Flat White" => {rate: 0.5, amount: 300},
+      :bill => {rate: 0.05, threshold: 5000, amount: 265}
     }
   end
 
