05df9a2f0c39ae7ce03540bc20e06c2bf3f9615a
TJQKAS <A26052013@GMAIL.COM>
2015-12-13 02:42:55 +0000

    update interface

--- app/app.rb
+++ app/app.rb
@@ -10,6 +10,7 @@ class Apps < Sinatra::Base
 enable :sessions
 set :views, proc { File.join(root, 'views') }
 
+
 get '/' do
   "Hello World"
   redirect to('/menu')
@@ -35,6 +36,8 @@ get '/new_menu' do
     @name = data.item.to_s
     @quant = data.quantity.to_i
     @m.order(@name, @quant)
+    orderedlists = @m.array_m
+    session[:orderedlists]=orderedlists
   end
     @price_order=@m.prices
     sum_order=@m.sum
@@ -42,9 +45,12 @@ get '/new_menu' do
     @taxes_order=@m.taxes
     @discount_order=@m.discount
     @totals=@m.total_bill_show
+    session[:totals]=@totals
     @totals.each do | total |
     puts @total
     @total
+
+
     end
   erb :new_menu
 end
@@ -71,12 +77,23 @@ end
 end
 
  get '/finish' do
+  totals=session[:totals]
+  orderedlists=session[:orderedlists]
+  payit = session[:payit]
+  sum_order = session[:sum_order]
+  @change = payit.to_f - sum_order.to_f
+  puts payit
+  puts sum_order
+  puts orderedlists
+  puts @change
+  # @orderedlists = orderedlists.to_a
+  # @orderedlists.each do |orderedlist|
+  # puts @orderedlist
+  puts totals
    erb :finish
  end
 
- DatabaseCleaner.cleaning do
-   Database
- end
+
 
 
 
