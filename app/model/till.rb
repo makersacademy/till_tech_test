7836064aa04d6b50c5ac48bfefc9f813b27b88bc
ALRW <awerner1@googlemail.com>
2016-01-14 13:34:22 +0000

    Added take payment method

--- app/model/till.rb
+++ app/model/till.rb
@@ -1,22 +1,44 @@
 class Till
-  attr_reader :data
-  def initialize(data)
-    @data = data[0]
-  end
+  attr_reader :data, :tax, :order_total
+  DEFAULT_TAX = 8.64
 
-  def print_menu
-    menu.each{|key, value| "#{key}, #{value}"}
+  def initialize(data, tax = DEFAULT_TAX)
+    @data = data[0]
+    @tax = tax
+    @order_total = 0
   end
 
   def calculate_line_total(line)
     line[get_line_key(line)]*menu[get_line_key(line).to_s]
   end
 
-  def calculate_total(order)
+  def calculate_base_total(order)
     total = 0
     order[:items].each{|order_item| total += calculate_line_total(order_item)}
     total.round(2)
   end
+
+  def calculate_tax(order_amount)
+    (order_amount * to_percent(tax)).round(2)
+  end
+
+  def total(order)
+    base = calculate_base_total(order)
+    tax = calculate_tax(base)
+    @order_total = (base + tax).round(2)
+  end
+
+  def take_payment(amount)
+    change = 0
+    if amount > order_total
+      change = amount - order_total
+      @order_total = 0
+    else
+      fail "The amount given is less than the cost of the order"
+    end
+    change.round(2)
+  end
+
 
   private
 
@@ -26,6 +48,10 @@ class Till
 
   def get_line_key(line)
     line.keys[0]
+  end
+
+  def to_percent(number)
+    number / 100
   end
 
 end
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -1,37 +1,18 @@
 require_relative '../app/model/till'
 require 'json'
 describe Till do
-    data = File.open("hipstercoffee.json", "r").read
-    data = JSON.parse(data)
+
+  data = JSON.parse(File.open("hipstercoffee.json", "r").read)
   subject(:till){described_class.new(data)}
-    line = {"Cafe Latte": 2}
-    order = {
-      name: "Jane",
-      items: [
-        {"Cafe Latte": 2},
-        {"Blueberry Muffin": 1},
-        {"Choc Mudcake": 1}
-      ]
-    }
-  context "when supplied resaurant data" do
-    it "can return the menu for the restaurant" do
-      expect(till.print_menu).to eq("Cafe Latte"=>4.75,
-        "Flat White"=>4.75,
-        "Cappucino"=>3.85,
-        "Single Espresso"=>2.05,
-        "Double Espresso"=>3.75,
-        "Americano"=>3.75,
-        "Cortado"=>4.55,
-        "Tea"=>3.65,
-        "Choc Mudcake"=>6.4,
-        "Choc Mousse"=>8.2,
-        "Affogato"=>14.8,
-        "Tiramisu"=>11.4,
-        "Blueberry Muffin"=>4.05,
-        "Chocolate Chip Muffin"=>4.05,
-        "Muffin Of The Day"=>4.55)
-    end
-  end
+  order = {
+    name: "Jane",
+    items: [
+      {"Cafe Latte": 2},
+      {"Blueberry Muffin": 1},
+      {"Choc Mudcake": 1}
+    ]
+  }
+  line = order[:items][0]
 
   describe "#calculate_line_total" do
     it "calculates the total cost of one line of an order" do
@@ -39,9 +20,38 @@ describe Till do
     end
   end
 
-  describe "#calculate_total" do
-    it "calculates the total cost of an order" do
-      expect(till.calculate_total(order)).to eq(19.95)
+  describe "#calculate_base_total" do
+    it "calculates the total cost of an order before tax" do
+      expect(till.calculate_base_total(order)).to eq(19.95)
+    end
+  end
+
+  describe "#calculate_tax" do
+    it "calculates the amount of tax payable on an order" do
+      base_total = till.calculate_base_total(order)
+      expect(till.calculate_tax(base_total)).to eq(1.72)
+    end
+  end
+
+  describe "#total" do
+    it "return the full total" do
+      expect(till.total(order)).to eq(21.67)
+    end
+  end
+
+  describe "#take_payment(amount)" do
+    before(:each) do
+      till.total(order)
+    end
+    it "resets the bill" do
+      till.take_payment(25)
+      expect(till.order_total).to eq(0)
+    end
+    it"returns the correct change" do
+      expect(till.take_payment(25)).to eq(3.33)
+    end
+    it "alerts the operator if incorrect payment is made" do
+      expect{till.take_payment(20)}.to raise_error("The amount given is less than the cost of the order")
     end
   end
 end
