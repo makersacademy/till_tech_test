ff8039f14ccfaea956ce7d4d963acd31d2d1e3b3
Mattia46 <mattia.assogna@gmail.com>
2016-01-14 13:45:17 +0000

    added the total function with tax

--- spec/till.js
+++ spec/till.js
@@ -27,27 +27,60 @@ describe("Till", function(){
           "Muffin Of The Day": 4.55
         }
       ]
-    };
+      };
 
-    till = new Till(menu);
-  });
+      till = new Till(menu);
+    });
 
-  it('Should has a ShopName', function(){
-    console.log(till.shopName);
-    console.log(menu);
-    expect(till.shopName).toBe("The Coffee Connection");
-  });
+    it('Should has a ShopName', function(){
+      console.log(till.shopName);
+      console.log(menu);
+      expect(till.shopName).toBe("The Coffee Connection");
+    });
 
-  it('Should has a address', function(){
-    expect(till.address).toBe("123 Lakeside Way");
-  });
+    it('Should has a address', function(){
+      expect(till.address).toBe("123 Lakeside Way");
+    });
 
-  it('Should has a phone number', function(){
-    expect(till.phone).toBe("16503600708");
-  });
+    it('Should has a phone number', function(){
+      expect(till.phone).toBe("16503600708");
+    });
 
-  it('Should has a price for a Cappuccino', function(){
-    console.log(till.prices.Cappucino);
-    expect(till.prices.Cappucino).toEqual(3.85);
+    it('Should has a price for a Cappuccino', function(){
+      console.log(till.prices.Cappucino);
+      expect(till.prices.Cappucino).toEqual(3.85);
+    });
+
+    describe('new order', function(){
+      it('can create an order', function(){
+        console.log('order', till.order);
+        expect(till.order).not.toBe(null);
+      });
+
+      it('can add beverage to his order', function(){
+        till.add(till.prices.Cappucino);
+        console.log(till.order);
+        expect(till.order.length).toBe(1);
+      });
+    });
+
+    describe('an User can create a pay the order', function(){
+
+      beforeEach(function(){
+        till.add(till.prices.Cappucino);
+        till.add(till.prices.Tea);
+      });
+
+      it('can add many beverages to his order', function(){
+        expect(till.order.length).toBe(2);
+        console.log(till.order);
+      });
+
+      it('has a bill', function(){
+        till.total();
+        expect(till.sum).toBe(8.148);
+        console.log('bill', till.sum);
+      });
+
+    });
   });
-});
