c8a13090a691a73a993337a0934cc8b635e1d9de
ALRW <awerner1@googlemail.com>
2016-01-14 12:24:57 +0000

    added total methods

--- app/model/till.rb
+++ app/model/till.rb
@@ -5,13 +5,27 @@ class Till
   end
 
   def print_menu
-    menu.each{|key, value| p "#{key}, #{value}"}
+    menu.each{|key, value| "#{key}, #{value}"}
+  end
+
+  def calculate_line_total(line)
+    line[get_line_key(line)]*menu[get_line_key(line).to_s]
+  end
+
+  def calculate_total(order)
+    total = 0
+    order[:items].each{|order_item| total += calculate_line_total(order_item)}
+    total.round(2)
   end
 
   private
 
   def menu
     data["prices"][0]
+  end
+
+  def get_line_key(line)
+    line.keys[0]
   end
 
 end
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -4,7 +4,15 @@ describe Till do
     data = File.open("hipstercoffee.json", "r").read
     data = JSON.parse(data)
   subject(:till){described_class.new(data)}
-
+    line = {"Cafe Latte": 2}
+    order = {
+      name: "Jane",
+      items: [
+        {"Cafe Latte": 2},
+        {"Blueberry Muffin": 1},
+        {"Choc Mudcake": 1}
+      ]
+    }
   context "when supplied resaurant data" do
     it "can return the menu for the restaurant" do
       expect(till.print_menu).to eq("Cafe Latte"=>4.75,
@@ -22,6 +30,18 @@ describe Till do
         "Blueberry Muffin"=>4.05,
         "Chocolate Chip Muffin"=>4.05,
         "Muffin Of The Day"=>4.55)
+    end
+  end
+
+  describe "#calculate_line_total" do
+    it "calculates the total cost of one line of an order" do
+      expect(till.calculate_line_total(line)).to eq(9.50)
+    end
+  end
+
+  describe "#calculate_total" do
+    it "calculates the total cost of an order" do
+      expect(till.calculate_total(order)).to eq(19.95)
     end
   end
 end
