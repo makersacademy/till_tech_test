ba3f5849c25592f8e13df003f86df301046db656
danwheeler <da-wheeler@hotmail.co.uk>
2016-01-16 12:26:22 +0000

    changed port

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ app.get('/', function (req, res) {
   res.render('./index');
 });
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8000));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
