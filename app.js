ab571dd1c12216db3d9d3909ffd8ee756fb2987e
danwheeler <da-wheeler@hotmail.co.uk>
2016-01-16 12:46:48 +0000

    added console log

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ app.use(express.static(__dirname + '/'));
 app.set('views', __dirname + '/');
 
 app.get('/', function (req, res) {
-  res.render('./index');
+  res.render('./index.html');
 });
 
 app.set('port', (process.env.PORT || 5000));
-
+console.log(app.get('port');
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
