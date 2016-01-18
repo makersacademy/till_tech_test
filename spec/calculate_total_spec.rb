40a774e210f137cb03839f03daf85b04ce626a77
deonjtan@gmail.com <deonjtan@gmail.com>
2016-01-18 12:16:12 +0000

    add ruby version to travis.yml

--- spec/calculate_total_spec.rb
+++ spec/calculate_total_spec.rb
@@ -9,7 +9,7 @@ describe CalculateTotal do
     allow(order).to receive(:check_order).and_return({cafe_latte: 2, blueberry_muffin: 1})
   end
 
-  context "when passing an order to CalculateOrder" do
+  context "when passing an order to CalculateTotal" do
     it "returns a subtotal per order line, the total tax and the total value of the order" do
       subtotals = {cafe_latte: "2 x $9.50", blueberry_muffin: "1 x $4.05"}
       expect(described_class.run(prices, order)).to eq({subtotal: subtotals, tax: "$1.17", total: "$14.72"})
