98a59daa3ccb0137cf379ca6fd670d3b89269284
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-21 18:09:34 +0100

    made attributes private and refactored sum method

--- app/order.py
+++ app/order.py
@@ -1,8 +1,20 @@
 class Order(object):
 
-    def __init__(self, prices):
-        self.items = {}
-        self.prices = prices
+    def __init__(self, menu):
+        self._items = {}
+        self._menu = menu
+        self._prices = []
+
+    def show_menu(self):
+        return self._menu
 
     def add_item(self, item, quantity = 1):
-        self.items[item] = quantity
+        self._items[item] = quantity
+
+    def sum_items(self):
+        self._calcaulate_each_item()
+        return sum(self._prices)
+
+    def _calcaulate_each_item(self):
+        for item, quantity in self._items.items():
+            self._prices.append(self._menu[item] * quantity)
--- tests/test_order.py
+++ tests/test_order.py
@@ -5,20 +5,13 @@ from app.order import Order
 class OrderTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.sample_prices = { "Latte": 4.75, "Capuccino": 3.85 }
-        self.order = Order(self.sample_prices)
+        self.sample_menu = { "Latte": 4.75, "Capuccino": 3.85 }
+        self.order = Order(self.sample_menu)
 
-    def test_items_default_empty(self):
-        self.assertEqual(self.order.items, {})
+    def test_initializes_with_menu(self):
+        self.assertEqual(self.order.show_menu(), self.sample_menu)
 
-    def test_initializes_with_prices(self):
-        self.assertEqual(self.order.prices, self.sample_prices)
-
-    def test_add_item(self):
-        self.order.add_item("Latte", 2)
-        self.assertEqual(self.order.items, { "Latte": 2 })
-
-    def test_sum_items(self):
+    def test_add_and_sum_items(self):
         self.order.add_item("Latte", 2)
         self.order.add_item("Capuccino", 2)
-        self.assertEqual(self.order.sum(), 17.2)
+        self.assertEqual(self.order.sum_items(), 17.2)
