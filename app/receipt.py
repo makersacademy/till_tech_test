6074307cc6477f392d645aceb9f2e4a62f83aab9
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-21 20:17:09 +0100

    receipt output as json

--- app/receipt.py
+++ app/receipt.py
@@ -1,3 +1,5 @@
+import json
+
 class Receipt(object):
 
     def __init__(self, order, menu):
@@ -5,4 +7,17 @@ class Receipt(object):
         self._menu = menu
 
     def output_as_json(self):
-        return
+        receipt = {}
+        for item in self._order.show_items():
+            receipt[item] = self._format_line(item)
+        receipt["Tax"] = self._format_currency(self._order.calculate_tax())
+        receipt["Total"] = self._format_currency(self._order.total())
+        return json.dumps(receipt)
+
+    def _format_line(self, item):
+        quantity = self._order.show_items()[item]
+        price = self._menu[item]
+        return "{} x ${}".format(quantity, price)
+
+    def _format_currency(self, price):
+        return "${}".format(price)
--- tests/test_receipt.py
+++ tests/test_receipt.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from unittest.mock import MagicMock
 from app.receipt import Receipt
 
@@ -10,13 +11,13 @@ class ReceiptTestCase(unittest.TestCase):
         self.receipt = Receipt(self.order, menu)
 
     def test_output_as_json(self):
-        self.order.show_items = MagicMock(return_value = { "Latte": 1, "Capuccino": 2})
+        self.order.show_items = MagicMock(return_value = { "Latte": 1, "Capuccino": 2 })
         self.order.calculate_tax = MagicMock(return_value = 1.08)
         self.order.total = MagicMock(return_value = 13.53)
         expected_output = {
             "Latte": "1 x $4.75",
-            "Capuccio": "2 x $3.85",
-            "Tax": "1.08",
+            "Capuccino": "2 x $3.85",
+            "Tax": "$1.08",
             "Total": "$13.53"
         }
-        self.assertEqual(self.output_as_json, expected_output)
+        self.assertEqual(self.receipt.output_as_json(), json.dumps(expected_output))
