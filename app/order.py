0c865559da6bb4b77d05fc9c6e8b10a9ade6a4be
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-21 18:54:00 +0100

    made tax calculation public. can calculate total

--- app/order.py
+++ app/order.py
@@ -13,9 +13,14 @@ class Order(object):
         self._calcaulate_each_item()
         return sum(self._prices)
 
+    def calculate_tax(self):
+        tax = self.sum_items() * self._DEFAULT_TAX_RATE
+        return round(tax, 2)
+
+    def total(self):
+        total = self.sum_items() * (1 + self._DEFAULT_TAX_RATE)
+        return round(total, 2)
+
     def _calcaulate_each_item(self):
         for item, quantity in self._items.items():
             self._prices.append(self._menu[item] * quantity)
-
-    def _calculate_tax(self):
-        
--- tests/test_order.py
+++ tests/test_order.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest.mock import MagicMock
 from app.order import Order
 
 class OrderTestCase(unittest.TestCase):
@@ -7,8 +6,14 @@ class OrderTestCase(unittest.TestCase):
     def setUp(self):
         self.sample_menu = { "Latte": 4.75, "Capuccino": 3.85 }
         self.order = Order(self.sample_menu)
-
-    def test_add_and_sum_items(self):
-        self.order.add_item("Latte", 1)
+        self.order.add_item("Latte")
         self.order.add_item("Capuccino", 2)
+
+    def test_sum_items(self):
         self.assertEqual(self.order.sum_items(), 12.45)
+
+    def test_calculate_tax(self):
+        self.assertEqual(self.order.calculate_tax(), 1.08)
+
+    def test_total(self):
+        self.assertEqual(self.order.total(), 13.53)
