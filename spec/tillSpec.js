1751e53c8e7afd5dfcc9b835143a2bf70d99ae27
paulwallis42 <paulwallis42@gmail.com>
2017-01-10 11:13:20 +0000

    can add an order test passes

--- spec/tillSpec.js
+++ spec/tillSpec.js
@@ -13,7 +13,19 @@ describe("Till", function() {
 
   it('can store the table number', function(){
     till.addTableNumber(4);
-    expect(till.tableNumber()).toEqual(4)
+    expect(till.tableNumber()).toEqual(4);
   })
+
+  it('can add an item from the menu', function(){
+    till.addOrder('Cafe Latte');
+    expect(till.ordersList()).toEqual({'Cafe Latte': [ 1, 4.75 ]});
+  })
+
+  it('can have numerous of the same order', function(){
+    for ( i = 0; i < 5; i++ ) {
+      till.addOrder('Cafe Latte');
+    }
+    expect(till.ordersList()).toEqual({'Cafe Latte': [ 5, 4.75 ]})
+  });
 
 });
--- src/till.js
+++ src/till.js
@@ -1,6 +1,7 @@
 function Till() {
   this.customers = [];
   this.tableNum = null;
+  this.orders = {};
 };
 
 Till.prototype.addCustomerName = function (name) {
@@ -17,4 +18,19 @@ Till.prototype.addTableNumber = function (tableNumber) {
 
 Till.prototype.tableNumber = function () {
   return this.tableNum;
+};
+
+Till.prototype.addOrder = function (order) {
+  var quantity;
+  if (order in this.orders) {
+    quantity = this.orders[order][0];
+    quantity += 1;
+    this.orders[order][0] = quantity;
+  } else {
+    this.orders[order] = [ 1, menu[0].prices[0][order] ];
+  }
+};
+
+Till.prototype.ordersList = function () {
+  return this.orders;
 };
