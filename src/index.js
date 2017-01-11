e9259b9db5f518be6a24c63f4dfc4b1ebff17817
paulwallis42 <paulwallis42@gmail.com>
2017-01-11 00:40:22 +0000

    dynamic generation of html options

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ $(document).ready(function() {
       till.addCustomerName($('#customer-name').val());
       till.addTableNumber($('#table-number').val());
       event.preventDefault();
+      displayMenuSelections();
+      $('#start-button').hide();
     });
+
+    var displayMenuSelections = function(){
+      var menuSelection = menu[0].prices[0];
+      var menuItems = Object.keys(menuSelection);
+      menuItems.forEach(menuFunction);
+    }
+
+    function menuFunction(item){
+      $('#menu').append($('<option value="' + item + '">' + item + '</option>'));
+    }
+
 
 });
--- src/till.js
+++ src/till.js
@@ -94,7 +94,7 @@ Till.prototype.muffinDiscount = function () {
   var discount;
   var orderTotal = 0;
   for (order in this.orders) {
-    if (order == "Blueberry Muffin" || order == "Chocolate Chip Muffin") {
+    if (order == "Blueberry Muffin" || order == "Chocolate Chip Muffin" || order == "Muffin of The Day") {
       orderTotal += this.orders[order][0] * this.orders[order][1];
     }
   }
